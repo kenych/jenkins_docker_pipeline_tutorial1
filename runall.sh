2bfeba7d0a4df23c436d7d6b9729f6612e01450c
Kayan Azimov <kayan.azimov@wipro.com>
2017-07-17 23:34:42 +0100

    improve runall

--- runall.sh
+++ runall.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+#check your ports are free
+#sudo lsof -i tcp:8080 &&  sudo lsof -i tcp:9001
+
+jenkins_port=8080
+sonar_port=9001
+
 docker pull jenkins:2.60.1
 docker pull sonarqube:6.3.1
 
@@ -10,9 +16,11 @@ if [ ! -d downloads ]; then
     curl -o downloads/apache-maven-3.5.0-bin.tar.gz http://apache.mirror.anlx.net/maven/maven-3/3.5.0/binaries/apache-maven-3.5.0-bin.tar.gz
 fi
 
+docker stop mysonar myjenkins
+
 docker build -t myjenkins .
 
-docker run  -p 9000:9000 --rm --name mysonar sonarqube:6.3.1 &
+docker run  -p ${sonar_port}:9000 --rm --name mysonar sonarqube:6.3.1 &
 
 IP=$(ifconfig en0 | awk '/ *inet /{print $2}')
 
@@ -22,13 +30,8 @@ if [ ! -d m2deps ]; then
     mkdir m2deps
 fi
 
-docker run -p 8080:8080  -v `pwd`/downloads:/var/jenkins_home/downloads \
-    -v `pwd`/jobs:/var/jenkins_home/jobs/ -v `pwd`/m2deps:/var/jenkins_home/.m2/repository/ --rm --name myjenkins \
-    -e SONARQUBE_HOST=http://"${IP}":9000 myjenkins:latest
-
-
-
-
-
-
-
+docker run -p ${jenkins_port}:8080  -v `pwd`/downloads:/var/jenkins_home/downloads \
+    -v `pwd`/jobs:/var/jenkins_home/jobs/ \
+    -v `pwd`/m2deps:/var/jenkins_home/.m2/repository/ --rm --name myjenkins \
+    -e SONARQUBE_HOST=http://${IP}:${sonar_port} \
+    myjenkins:latest
